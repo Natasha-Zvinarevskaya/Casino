bd36ff344a0fed9c98ed23d9c4d43a1187eed495
natasha <natali-zve@mail.ru>
2025-08-28 15:21:16 +0300

    test conflict3

--- Services/RouletteGameService.cs
+++ Services/RouletteGameService.cs
@@ -33,8 +33,8 @@ namespace Casino.Services
         {
             Random rand = new Random();
             int start = rand.Next(0, roulette.Count);
-            var test = "256pO";
-            var b = "ji";
+            var test = "pO";
+           
             Sector droppSector = roulette[start];
             int i = rand.Next(10, 50);
             for (; i > 0;)
