8c98d229731c13123d42418721077f75f775586a
natasha <natali-zve@mail.ru>
2025-08-28 15:06:05 +0300

    conflict2

--- Services/RouletteGameService.cs
+++ Services/RouletteGameService.cs
@@ -34,6 +34,7 @@ namespace Casino.Services
             Random rand = new Random();
             int start = rand.Next(0, roulette.Count);
             var test = "256";
+            var b = "ji";
             Sector droppSector = roulette[start];
             int i = rand.Next(10, 50);
             for (; i > 0;)
